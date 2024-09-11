9a5cde3ec9444ef7c2726841889fad716e5ee612
s-heppner <mail@s-heppner.com>
2024-09-11 08:51:14 +0200

    service: Add option to modify listen address
    Previously, uvicorn only listened on `0.0.0.0`, which may be a
    security risk in some situations.
    
    We add a configuration option to modify the listen address via
    the `config.ini` and set the default to `127.0.0.1`

--- semantic_matcher/service.py
+++ semantic_matcher/service.py
@@ -180,4 +180,4 @@ if __name__ == '__main__':
     APP.include_router(
         SEMANTIC_MATCHING_SERVICE.router
     )
-    uvicorn.run(APP, host="0.0.0.0", port=int(config["SERVICE"]["PORT"]))
+    uvicorn.run(APP, host=config["SERVICE"]["LISTEN_ADDRESS"], port=int(config["SERVICE"]["PORT"]))
